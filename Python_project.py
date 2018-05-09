5ca4d78a4bfdd258d15bd993940a2d39d5271451
Marion Hutinel <marion.hutinel@gu.se>
2018-05-09 11:44:58 +0200

    Reorganised step

--- Python_project.py
+++ Python_project.py
@@ -1,10 +1,10 @@
-From the fasta file separate the 16S and the resistance gene part of the sequences.
-Load the data into python(open or with biopython load as sequence object)
-Either slice the sequence according to the known sequence or at the expected length (82)
-Write either 2 different files with one part of the sequence each or a new file with the 2 parts of the sequence separated (list[NDM,16S]).
-(eliminate the primer sequences)
-(create OTUs)
-Create clusters based on similarity (unix)
-Find a consensus seq for each cluster
-Blast the 16S to a 16S database (silva et/NCBI)
-Create a phylogenetic tree
+#From the fasta file separate the 16S and the resistance gene part of the sequences.
+    #Load the data into python(open or with biopython load as sequence object)
+    #Either slice the sequence according to the known sequence or at the expected length (82)
+    #Make a new file with the 2 parts of the sequence separated (seq_name[NDM,16S]).
+#(eliminate the primer sequences)
+#create OTUs (unix)
+    #Create clusters based on similarity 
+    #Find consensus seq for each cluster
+#Blast the consensus seq of each OTU to a 16S database (silva and/or NCBI)
+#Create a phylogenetic tree
