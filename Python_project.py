46046a7dcc6a2c62cdc325bdb07f7d204dcd2771
Marion Hutinel <marion.hutinel@gu.se>
2018-05-15 16:18:37 +0200

    added filter out short sequences and first option to split sequences

--- Python_project.py
+++ Python_project.py
@@ -1,7 +1,39 @@
-#From the fasta file separate the 16S and the resistance gene part of the sequences.
-    #Load the data into python(open or with biopython load as sequence object)
-    #Either slice the sequence according to the known sequence or at the expected length (82)
-    #Make a new file with the 2 parts of the sequence separated (seq_name[NDM,16S]).
+#Convert fastq file into fasta file
+from Bio import SeqIO
+count=SeqIO.convert("Pune-C3.assembled.fastq","fastq","Pune-C3.assembled.fasta","fasta")
+print("converted %i records" % count)
+
+#Filter out short sequences (<300)
+input_filename = "Pune-C3.assembled.fasta"
+output_filename = "len_filtered_Pune-C3.assembled.fasta"
+min_len = 300
+count = 0
+total = 0
+output_handle = open(output_filename, "w")
+for record in SeqIO.parse(input_filename, "fasta"):
+    total += 1
+    if len(record) >= min_len:
+        count += 1
+        SeqIO.write(record, output_handle, "fasta")
+output_handle.close()
+print(" %i records selected out of %i " % (count, total))
+
+#filter out unique sequences
+        
+#Separate the 16S and the resistance gene part of the sequences by slicing the sequence according to the known sequence or at the expected length (82)
+    #Make a new file with lists called by the seqID ex:seq_name[NDM,16S]
+input_filename = "len_filtered_Pune-C3.assembled.fasta"
+output_filename = 
+len_Rpart = 82 
+output_handle = open(output_filename, "w")
+for record in SeqIO.parse(input_filename, "fasta"):
+    record.id=[record.seq[:len_Rpart], record.seq[len_Rpart:]]
+    SeqIO.write(record.id, output_handle, "fasta")
+output_handle.close()
+    #OR create 2 files 
+    
+    
+#Eliminate the sequences that do not have a NDM sequence
 #(eliminate the primer sequences)
 #create OTUs (unix)
     #Create clusters based on similarity 
