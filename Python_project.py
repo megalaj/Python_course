db2eb1d9d98fbca5d2daeb7871e6591cbda8c24f
Marion Hutinel <marion.hutinel@gu.se>
2018-05-20 15:34:54 +0200

    Functions

--- Python_project.py
+++ Python_project.py
@@ -1,55 +1,49 @@
 #Convert fastq file into fasta file
-from Bio import SeqIO
-count=SeqIO.convert("../Pune-C3.assembled.fastq","fastq","../Pune-C3.assembled.fasta","fasta")
-print("converted %i records" % count)
+def fastq_to_fasta(input_file, output_file):
+        from Bio import SeqIO
+        count=SeqIO.convert("input_file","fastq","output_file","fasta")
+        print("converted %i records" % count)
 
-#Filter out short sequences (<300)
-from Bio import SeqIO
-input_filename = "../Pune-C3.assembled.fasta"
-output_filename = "../len_filtered_Pune-C3.assembled.fasta"
-min_len = 300
-count = 0
-total = 0
-output_handle = open(output_filename, "w")
-for record in SeqIO.parse(input_filename, "fasta"):
-    total += 1
-    if len(record) >= min_len:
-        count += 1
-        SeqIO.write(record, output_handle, "fasta")
-output_handle.close()
-print(" %i records selected out of %i " % (count, total))
-
-#filter out unique sequences (usearch?)
+#Filter out short sequences
+def filter_short(input_file, output_file, min_length):
+    from Bio import SeqIO
+    count = 0
+    total = 0
+    output_handle = open(output_file, "w")
+    for record in SeqIO.parse(input_file, "fasta"):
+        total += 1
+        if len(record) >= min_length:
+            count += 1
+            SeqIO.write(record, output_handle, "fasta")
+    output_handle.close()
+    print(" %i records selected out of %i " % (count, total))
         
-#Separate the 16S and the resistance gene part of the sequences by slicing the sequence according to the known sequence or at the expected length (82)
-    #Make a new file with lists called by the seqID ex:seq_name[NDM,16S]
-input_filename = "len_filtered_Pune-C3.assembled.fasta"
-output_filename = "split_list"
-len_Rpart = 82 
-output_handle = open(output_filename, "w")
-for record in SeqIO.parse(input_filename, "fasta"):
-    Rgene = str(record.id) + "_Rgene" + "\n" + str(record.seq[:len_Rpart])
-    part16S =  str(record.id) + "_16S" + "\n" + str(record.seq[len_Rpart:])
-    SeqIO.write(Rgene, output_handle, "fasta")
-    SeqIO.write(part16S, output_handle, "fasta")
-output_handle.close()
-    #OR create 2 files 
-input_filename = "len_filtered_Pune-C3.assembled.fasta"
-output_Rgene =
-output_16S =
-len_Rpart = 82 
-output_handle = open(output_Rgene, "w")
-for record in SeqIO.parse(input_filename, "fasta"):
-    record.seq = record.seq[:len_Rpart]
-    SeqIO.write(record, output_handle, "fasta")
-output_handle.close()   
-output_handle = open(output_16S, "w")
-for record in SeqIO.parse(input_filename, "fasta"):
-    record.seq = record.seq[len_Rpart:]
-    SeqIO.write(record, output_handle, "fasta")
-output_handle.close()  
+#Cut the sequences at the expected length of the first part
+    #Duplicate the headers and associate each one with a part of the sequence
+def slice_seq(input_file, output_file, len_first_part):
+    from Bio import SeqIO
+    output_handle = open(output_file, "w")
+    for record in SeqIO.parse(input_file, "fasta"):
+        partA = str(record.id) + "_A" + "\n" + str(record.seq[:len_first_part])
+        partB =  str(record.id) + "_B" + "\n" + str(record.seq[len_first_part:])
+        SeqIO.write(partA, output_handle, "fasta")
+        SeqIO.write(partB, output_handle, "fasta")
+    output_handle.close()
+    #OR create 2 files with on part of the sequence in each
+def slice_seq_2(input_file, output_file_A, output_file_B, len_first_part):
+    from Bio import SeqIO
+    output_handle = open(output_file_A, "w")
+    for record in SeqIO.parse(input_file, "fasta"):
+        record.seq = record.seq[:len_first_part]
+        SeqIO.write(record, output_handle, "fasta")
+    output_handle.close()   
+    output_handle = open(output_file_B, "w")
+    for record in SeqIO.parse(input_file, "fasta"):
+        record.seq = record.seq[len_first_part:]
+        SeqIO.write(record, output_handle, "fasta")
+    output_handle.close()  
+    
 #Eliminate the sequences that do not have the sequence of the resistance gene
-    #
 #(eliminate the primer sequences)
 #create OTUs (usearch -cluster_fast)
     #Create clusters based on similarity 
